b35d0b0606836c6076508cba1f9b303fe72bbfee
Kieran Hitchcock <kieran.hitchcock@rocketmail.com>
2024-06-22 22:26:09 +1200

    Toggle Face Up

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -19,21 +19,22 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     
     var body: some View{
         ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻")
                     .font(.title)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
             
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
